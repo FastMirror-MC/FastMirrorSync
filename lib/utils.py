4e815acecf2658044823e110fed5b7724b6db517
Skeleton321 <skeleton_321@163.com>
2022-03-21 20:22:43 +0800

    🐞 fix: 移除不再使用的功能

--- lib/utils.py
+++ lib/utils.py
@@ -6,18 +6,15 @@ by IntelliJ IDEA
 """
 from __future__ import annotations
 
-import atexit
 import datetime
 import hashlib
-import sys
 import time
-import traceback
 
 import pytz
 
 from lib import log
 
-__all__ = ["datetime2str", "get_checksum", "if_file_invalid", "get_mills_time", "register_exit_event"]
+__all__ = ["datetime2str", "get_checksum", "if_file_invalid", "get_mills_time"]
 exit_event_queue = []
 
 
@@ -46,16 +43,3 @@ def if_file_invalid(stream, checksum: str, mode: str):
 
 def get_mills_time():
     return int(time.time() * 1000)
-
-
-def register_exit_event(func):
-    exit_event_queue.append(func)
-    return func
-
-
-@atexit.register
-def exit_handler():
-    exc_type, exc_value, exc_tb = sys.exc_info()
-    [invoke() for invoke in exit_event_queue]
-    if exc_value is not None:
-        traceback.print_exception(exc_type, exc_value, exc_tb)
