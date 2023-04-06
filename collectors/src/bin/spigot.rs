6340eedefa428e97e7999365a13ae3aa62597c38
qzwxsaedc <qzwxsaedc1@outlook.com>
2023-04-06 15:11:26 +0800

    fix(spigot): 修复spigot无法更新的问题

--- collectors/src/bin/spigot.rs
+++ collectors/src/bin/spigot.rs
@@ -5,7 +5,7 @@ use libcollector::{Collector, utils::split_once};
 const PROJECT_NAME: &str = "Spigot";
 
 fn main() -> Result<()> {
-    let jenkins = builder("http://localhost:14514")
+    let jenkins = builder("http://localhost:14514/job/Spigot/")
         .append_job("makefile")
         .version_selector(|_, api, _| { 
             let (mc_version, core_version) = split_once(&api.display_name, "-")?;
