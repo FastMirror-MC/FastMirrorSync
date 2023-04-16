9cf1b19d9cb11397c6ec9bccadd9fe1fe8e42e38
qzwxsaedc <qzwxsaedc1@outlook.com>
2023-04-16 14:05:14 +0800

    fix(catserver): 修复找不到asset的问题

--- collectors/src/bin/pufferfish.rs
+++ collectors/src/bin/pufferfish.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use collectors::api::jenkins::{builder, Api, Asset, AsCoreVersion};
+use collectors::api::jenkins::builder;
 use libcollector::Collector;
 
 const PROJECT_NAME: &str = "Pufferfish";
@@ -8,14 +8,14 @@ fn main() -> Result<()> {
     let jenkins = builder("https://ci.pufferfish.host")
         .append_job("Pufferfish-1.18")
         .append_job("Pufferfish-1.19")
-        .version_selector(|job: &'static str, api: &Api, _: &Asset| {
-            let mc_version = match job {
-                "Pufferfish-1.18" => "1.18.2",
-                "Pufferfish-1.19" => "1.19.4",
-                _ => return Err(anyhow::Error::msg(format!("unknown job name `{job}`")))
-            };
-            Ok((mc_version.to_string(), api.number.as_core_version()))
-        })
+        // .version_selector(|job: &'static str, api: &Api, _: &Asset| {
+        //     let mc_version = match job {
+        //         "Pufferfish-1.18" => "1.18.2",
+        //         "Pufferfish-1.19" => "1.19.4",
+        //         _ => return Err(anyhow::Error::msg(format!("unknown job name `{job}`")))
+        //     };
+        //     Ok((mc_version.to_string(), api.number.as_core_version()))
+        // })
         ;
     
     Collector::new(PROJECT_NAME)?.run(jenkins.build()?)
