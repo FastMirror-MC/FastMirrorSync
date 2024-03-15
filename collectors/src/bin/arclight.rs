22b26752b08642290624ad40d66972c573a6a33a
qzwxsaedc <qzwxsaedc1@outlook.com>
2024-03-15 13:38:32 +0800

    feat(arclight): arclight的多种ModLoader支持
    回头要重写，现在是屎上雕花

--- collectors/src/bin/arclight.rs
+++ collectors/src/bin/arclight.rs
@@ -2,20 +2,51 @@ use libcollector::{Collector, utils::split_once};
 use collectors::api::github::builder;
 
 use anyhow::Result;
+use anyhow::anyhow;
 
 const PROJECT_NAME: &str = "Arclight";
 
 fn main() -> Result<()> {
     let github = builder("IzzelAliz/Arclight")
-        .version_selector(|api| {
-            let binding = api.tag_name.to_string();
-            let (mc, core) = split_once(&binding, "/")?;
-            match mc.to_lowercase().as_str() {
-                "greathorn" => return Ok(("1.19.3".to_string(), core.to_owned())),
-                "horn"      => return Ok(("1.19.2".to_string(), core.to_owned())),
-                _           => return Ok((mc.to_owned(), core.to_owned()))
-            }
-        });
+    .version_selector(|api| {
+        let binding = api.tag_name.to_string();
+        let (mc, core) = split_once(&binding, "/")?;
+        return Ok((mc.to_owned(), format!("{}-{}", "forge", core)));
+    })
+    .asset_selector(|api| {
+        api.assets.iter().find(|o| o.name["arclight-".len()..].starts_with("forge")).ok_or(anyhow!("")).cloned()
+    });
+
+    if let Err(err) = Collector::new(PROJECT_NAME)?.run(github.build()?) {
+        println!("{}", err);
+    }
     
-    Collector::new(PROJECT_NAME)?.run(github.build()?)
+    let github = builder("IzzelAliz/Arclight")
+    .version_selector(|api| {
+        let binding = api.tag_name.to_string();
+        let (mc, core) = split_once(&binding, "/")?;
+        return Ok((mc.to_owned(), format!("{}-{}", "fabric", core)));
+    })
+    .asset_selector(|api| {
+        api.assets.iter().find(|o| o.name["arclight-".len()..].starts_with("fabric")).ok_or(anyhow!("")).cloned()
+    });
+
+    if let Err(err) = Collector::new(PROJECT_NAME)?.run(github.build()?) {
+        println!("{}", err);
+    }
+
+    let github = builder("IzzelAliz/Arclight")
+    .version_selector(|api| {
+        let binding = api.tag_name.to_string();
+        let (mc, core) = split_once(&binding, "/")?;
+        return Ok((mc.to_owned(), format!("{}-{}", "neoforge", core)));
+    })
+    .asset_selector(|api| {
+        api.assets.iter().find(|o| o.name["arclight-".len()..].starts_with("neoforge")).ok_or(anyhow!("")).cloned()
+    });
+
+    if let Err(err) = Collector::new(PROJECT_NAME)?.run(github.build()?) {
+        println!("{}", err);
+    }
+    Ok(())
 }
